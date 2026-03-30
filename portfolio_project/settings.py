df7e839cec1cd5bfe284bc2001c8e5466e9573c3
Gregorio N. Lapus <lapusgregorio05@gmail.com>
2026-03-31 06:57:26 +0800

    Updated settings.py for deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greglapus.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
